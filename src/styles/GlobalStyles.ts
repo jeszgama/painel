873f1873d6ab3a4e01759c5353475d4f89d13f62
“jessica” <jessicagama9827@gmail.com>
2021-12-02 13:19:51 -0400

    --ambiente preparado

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    color: #fff;
+  }
+
+  html, border-style, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    height: 100%;
+    width: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+  }
+  html {
+    background: #000 ;
+  }
+  :root {
+    
+  }
+`;
---
+++ src/styles/Icons.ts
@@ -0,0 +1,1 @@
+export { Loop } from 'styled-icons/open-iconic';
